e98fea01ac25ab6e01b688586a80b71b6d7b8fa0
Brenda <abrendakhenya@gmail.com>
2023-09-16 20:01:03 -0700

    test

--- client/admin/src/components/Pantalla.tsx
+++ client/admin/src/components/Pantalla.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 //import '../styless/Pantalla.css';
-=======
-// import '../styless/Pantalla.css';
->>>>>>> 410c88a3a2ef83bde1d5c44566264b8c6d5418ed
+
 
 interface PantallaProps {
   input: string;
